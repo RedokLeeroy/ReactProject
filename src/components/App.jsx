2d5140e4bc3248409c434be9272aea51d030ffe4
Oleksandr Abramov <aleksander.abramov@gmail.com>
2022-08-16 23:25:37 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import { Login } from './Login';
 import { Logining } from './Logining';
+import { Main } from './Main/Main';
 export const App = () => {
   return (
     <div>
       <Login></Login>
       <Logining></Logining>
+      <Main />
     </div>
   );
 };
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,117 @@
+import Button from 'components/common/button/button';
+import { useState } from 'react';
+import s from './Main.module.css';
+
+export const Main = () => {
+  const [description, setDescription] = useState('');
+  const [category, setCategory] = useState('');
+  const [sum, setSum] = useState('');
+
+  const products = [
+    'Продукты',
+    'Алкоголь',
+    'Развлечения',
+    'Здоровье',
+    'Транспорт',
+    'Всё для дома',
+    'Техника',
+    'Коммуналка и связь',
+    'Спорт и хобби',
+    'Образование',
+    'Прочее',
+  ];
+
+  const handleChangeForm = evt => {
+    const { value, name } = evt.target;
+    switch (name) {
+      case 'description':
+        setDescription(value);
+        break;
+      case 'category':
+        setCategory(value);
+        break;
+      case 'sum':
+        setSum(value);
+        break;
+      default:
+        return;
+    }
+  };
+
+  const handleResetForm = () => {
+    setDescription('');
+    setCategory('');
+    setSum('');
+  };
+
+  const handleSubmitForm = evt => {
+    evt.preventDefault();
+    console.log('submit:', description, category, sum);
+  };
+
+  return (
+    <div className={s.container}>
+      <button className={s.btnAccent}>EXPENSES</button>
+      <button className={s.btn}>INCOME</button>
+      <div className={s.contentContainer}>
+        <div className={s.formContainer}>
+          <form className={s.form} onSubmit={handleSubmitForm}>
+            <input
+              className={s.inputDate}
+              type="data"
+              placeholder="Wait for date"
+            />
+            <input
+              className={s.inputDescription}
+              placeholder="Product description"
+              type="text"
+              name="description"
+              value={description}
+              onChange={handleChangeForm}
+            />
+
+            <select
+              className={s.inputCategory}
+              name="category"
+              defaultValue={category}
+              onChange={handleChangeForm}
+            >
+              <option value="" disabled hidden>
+                Product category
+              </option>
+              {products.map((el, ind) => (
+                <option value={el} key={ind} className={s.option}>
+                  {el}
+                </option>
+              ))}
+            </select>
+
+            <input
+              className={s.inputSumm}
+              placeholder="0,00"
+              type="number"
+              name="sum"
+              value={sum}
+              onChange={handleChangeForm}
+            />
+            <Button
+              className={s.buttonContainer}
+              text={'INPUT'}
+              type={'submit'}
+              onClick={handleSubmitForm}
+            />
+            <Button
+              className={s.buttonContainer}
+              text={'CLEAR'}
+              onClick={handleResetForm}
+            />
+          </form>
+        </div>
+        <div className={s.tableContainer}>
+          <div className={s.prods}></div>
+          <div className={s.sumary}></div>
+        </div>
+      </div>
+    </div>
+  );
+};
