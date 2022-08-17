b0118a7231b46af8a7f46c979cc047daf3cdd09d
alyapolishchuk <alinochkarul081@gmail.com>
2022-08-17 12:59:23 +0300

    c

--- src/components/Background/BackgroundHome.jsx
+++ src/components/Background/BackgroundHome.jsx
@@ -1,13 +1,11 @@
+import css from './BackgroundHome.module.css';
 export const BackgroundHome = () => {
   return (
-    <section className="container">
-        <img className="logo" alt="logo" src="./img/logo.png" width="90" height="31"/>
-      <div className="div">
-        <img className="rectangle" alt="rectangle" src="./img/rectangle.png" width="1280" height="526" />
-        <img className="cabbage" alt="a lot of cabagge" src="./img/kapusta_second.png" />
-        <img class="title" alt="title" src="./img/title.png" width="377"/>
-        <img class="two-abbage" alt="cabagge" src="./img/kapusta_main-small.png" width="183" />
-      </div>
-    </section>
+    <div className="backgroundHome">
+      <div className="rectangle"></div>
+      <div className="cabbage"></div>
+      <div className="title"></div>
+      <div className="two-abbage"></div>
+    </div>
   );
 };
--- src/components/Background/BackgroundMain.jsx
+++ src/components/Background/BackgroundMain.jsx
@@ -1,11 +1,8 @@
 export const BackgroundHome = () => {
   return (
-    <section className="container">
-        <img className="logo" alt="logo" src="./img/logo.png" width="90" height="31"/>
         <div className="div">
-            <img className="rectangle" alt="rectangle" src="./img/rectangle.png" width="1280" height="526"/>
-            <img className="cabbage-second" alt="a lot of cabagge" src="./img/kapusta_second.png"/>
+            {/* <img className="rectangle" alt="rectangle" src="./img/rectangle.png" width="1280" height="526"/>
+            <img className="cabbage-second" alt="a lot of cabagge" src="./img/kapusta_second.png"/> */}
         </div>
-    </section>
   );
 };
