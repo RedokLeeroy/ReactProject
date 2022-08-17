374bb55fd122d56fafaa31f7021a0f526e65519f
Valentin787 <89661234+Valentin787@users.noreply.github.com>
2022-08-17 12:18:30 +0300

    balance_container

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,13 @@
+import BalanceContainer from './BalanceContainer/BalanceContainer';
 import { Login } from './Login';
 import { Logining } from './Logining';
 export const App = () => {
   return (
     <div>
       <Login></Login>
+     
       <Logining></Logining>
+      <BalanceContainer/>
     </div>
   );
 };
