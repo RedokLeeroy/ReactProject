b46a722e927e4564cd72a6e76ca2a173e935dc05
Taras Lapytskyi <84970031+laptar@users.noreply.github.com>
2022-08-17 14:14:50 +0300

    test

--- src/Page/Report.jsx
+++ src/Page/Report.jsx
@@ -1,10 +1,12 @@
+import BalanceContainer from '../components/BalanceContainer/BalanceContainer';
 import { ReportSummery } from 'components/ReportSummary/ReportSummary';
 import { ReporCategory } from 'components/ReportCategory/ReportCategory';
 const Report = () => {
   return (
     <>
-      <ReportSummery />
-      <ReporCategory />
+      <BalanceContainer />
+      {/* <ReportSummery />
+      <ReporCategory /> */}
     </>
   );
 };
