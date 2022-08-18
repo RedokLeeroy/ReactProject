2979d4a2591746dcf6ca674687f348982c4f328b
Yehor Boichenko <boichenko.yehor@gmail.com>
2022-08-18 14:56:45 +0300

    fix

--- src/components/ModalTeam/OurTeam.jsx
+++ src/components/ModalTeam/OurTeam.jsx
@@ -1,36 +1,36 @@
-import { Link } from 'react-router-dom';
-import data from './data';
-import styles from './Ourteam.module.css';
+// import { Link } from 'react-router-dom';
+// import data from './data';
+// import styles from './Ourteam.module.css';
 
-export default function OurTeam() {
-  return (
-    <div className={styles.content}>
-      <Link to="/" alt="homepage" className={styles.close} />
+// export default function OurTeam() {
+//   return (
+//     <div className={styles.content}>
+//       <Link to="/" alt="homepage" className={styles.close} />
 
-      {teams.map((team, idx) => (
-        <figure key={idx} className={styles.team}>
-          <img src={team.photo} alt="" className={styles.background} />
-          <img
-            src={data.photo}
-            alt={data.fullName}
-            className={styles.profile}
-          />
-          <figcaption>
-            <h3>
-              {team.fullName}
-              <span>{team.position}</span>
-            </h3>
-            <div className={styles.icons}>
-              <a href={team.linkedIn} target="_blank" rel="noreferrer">
-                <i className="ion-social-linkedin-outline"></i>
-              </a>
-              <a href={team.gitHub} target="_blank" rel="noreferrer">
-                <i className="ion-social-github-outline"></i>
-              </a>
-            </div>
-          </figcaption>
-        </figure>
-      ))}
-    </div>
-  );
-}
+//       {teams.map((team, idx) => (
+//         <figure key={idx} className={styles.team}>
+//           <img src={team.photo} alt="" className={styles.background} />
+//           <img
+//             src={data.photo}
+//             alt={data.fullName}
+//             className={styles.profile}
+//           />
+//           <figcaption>
+//             <h3>
+//               {team.fullName}
+//               <span>{team.position}</span>
+//             </h3>
+//             <div className={styles.icons}>
+//               <a href={team.linkedIn} target="_blank" rel="noreferrer">
+//                 <i className="ion-social-linkedin-outline"></i>
+//               </a>
+//               <a href={team.gitHub} target="_blank" rel="noreferrer">
+//                 <i className="ion-social-github-outline"></i>
+//               </a>
+//             </div>
+//           </figcaption>
+//         </figure>
+//       ))}
+//     </div>
+//   );
+// }
