cc84a4ea330f203d5bf9c5b5360d6cf7424e55b9
Oleksandr Abramov <aleksander.abramov@gmail.com>
2022-08-18 18:52:44 +0300

    done first gener

--- src/Page/Income.jsx
+++ src/Page/Income.jsx
@@ -1,5 +1,22 @@
+import { Main } from 'components/Main/Main';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { incomeCategories } from 'redux/income/income-operations';
+
 const Income = () => {
-  return <></>;
+  const isLogin = useSelector(state => state.auth.isLogin);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (isLogin) {
+      dispatch(incomeCategories());
+    }
+  }, [dispatch, isLogin]);
+  return (
+    <>
+      <Main />
+    </>
+  );
 };
 
 export default Income;
