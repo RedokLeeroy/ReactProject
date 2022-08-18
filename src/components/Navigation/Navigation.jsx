dbb75916336b7d672bccde89df9113ca700dfa31
Tasha1003Natasha <0955712824n@gmail.com>
2022-08-18 10:42:35 +0300

    Navigation готово

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,10 +1,10 @@
 import styles from './Navigation.module.css';
 import Sprite from '../../images/sprite.svg';
-// import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import UserMenu from 'components/UserMenu/UserMenu';
 
 const Navigation = () => {
-  // const isLogin = useSelector(state => state.auth.isLogin);
+  const isLogin = useSelector(state => state.auth.isLogin);
 
   return (
     <header className={styles.header}>
@@ -12,8 +12,8 @@ const Navigation = () => {
         <svg className={styles.logoIcon} width={90} height={31}>
           <use href={`${Sprite}#icon-logo`}></use>
         </svg>
-        {/* {isLogin && <UserMenu />} */}
-        <UserMenu />
+        {isLogin && <UserMenu />}
+        {/* <UserMenu /> */}
       </div>
     </header>
   );
--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,13 +1,13 @@
-// import { useDispatch, useSelector } from 'react-redux';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+// import { useDispatch } from 'react-redux';
 import { logOut } from 'redux/auth/auth-operations';
 // import { Link } from 'react-router-dom';
 import styles from './UserMenu.module.css';
 
 const UserMenu = () => {
   const dispatch = useDispatch();
-  // const userName = useSelector(state => state.auth?.user?.email);
-  const userName = 'User Name';
+  const userName = useSelector(state => state.auth?.user?.email);
+  // const userName = 'User Name';
   const avatarName = userName.slice(0, 1).toLocaleUpperCase();
 
   return (
