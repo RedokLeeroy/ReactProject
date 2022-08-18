7a26733e3bd2748476894909f16a1e94b517fd1f
Tasha1003Natasha <0955712824n@gmail.com>
2022-08-18 11:46:49 +0300

    Стилі mobile/tablet

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -3,16 +3,18 @@ import { useDispatch, useSelector } from 'react-redux';
 import { logOut } from 'redux/auth/auth-operations';
 // import { Link } from 'react-router-dom';
 import styles from './UserMenu.module.css';
+import Sprite from '../../images/sprite.svg';
 
 const UserMenu = () => {
   const dispatch = useDispatch();
   const userName = useSelector(state => state.auth?.user?.email);
+  // console.log(userName);
   // const userName = 'User Name';
-  const avatarName = userName.slice(0, 1).toLocaleUpperCase();
+  const avatarName = userName?.slice(0, 1).toLocaleUpperCase();
 
   return (
     <div className={styles.userMenu}>
-      <p className={styles.avatarName}>{avatarName}</p>
+      <p className={styles.avatarName}>{avatarName || 'U'}</p>
       <p className={styles.userName}>
         {userName || 'User Name'} <span className={styles.userLine}></span>
       </p>
@@ -23,6 +25,10 @@ const UserMenu = () => {
       >
         Exit
       </button>
+
+      <svg className={styles.logoutIcon} width={16} height={16}>
+        <use href={`${Sprite}#icon-logout`}></use>
+      </svg>
     </div>
   );
 };
