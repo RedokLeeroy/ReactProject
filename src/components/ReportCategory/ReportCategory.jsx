3aeea75bfa7bf7fc73faf3c1d3f6e533ac774b07
Taras Lapytskyi <84970031+laptar@users.noreply.github.com>
2022-08-18 18:12:08 +0300

    fix

--- src/components/ReportCategory/ReportCategory.jsx
+++ src/components/ReportCategory/ReportCategory.jsx
@@ -1,27 +1,44 @@
+import { useSelector } from 'react-redux';
+
 import s from './ReportCategory.module.css';
 import svg from '../../svgReport/svg-report.svg';
 import { ReportCategoryBtn } from 'components/ReportCategoryBtn/ReportCategoryBtn';
-import data from '../../data.json';
+import { useState } from 'react';
 
 export const ReporCategory = () => {
+  const getExpList = useSelector(state => state.currentPeriod.expense.data);
+  const getIncList = useSelector(state => state.currentPeriod.incomes.data);
+
+  const get = useSelector(state => state.currentPeriod);
+  const [currentBtn, setCurrentBtn] = useState(true);
+  const expInc = {
+    true: 'Expenses',
+    false: 'Income',
+  };
+  const handleTogleCurrentBtn = () => {
+    setCurrentBtn(prev => !prev);
+  };
+
+  console.log(getIncList);
   return (
     <div className={s.section}>
       <div className={s.btn}>
-        <button>
+        <button type="button" onClick={handleTogleCurrentBtn}>
           <svg width={10} height={10}>
             <use href={`${svg}#icon-btn-left`}></use>
           </svg>
         </button>
-        <p className={s.btnName}>Expenses</p>
-        <button>
+        <p className={s.btnName}>{expInc[currentBtn]}</p>
+        <button type="button" onClick={handleTogleCurrentBtn}>
           <svg width={10} height={10}>
             <use href={`${svg}#icon-btn-right`}></use>
           </svg>
         </button>
       </div>
+
       <ul className={s.ctegoryList}>
-        {data.map(el => (
-          <ReportCategoryBtn key={el.id} {...el} />
+        {Object.entries(currentBtn ? getExpList : getIncList).map(el => (
+          <ReportCategoryBtn key={el[0]} data={el} />
         ))}
       </ul>
     </div>
--- src/components/ReportCategoryBtn/ReportCategoryBtn.jsx
+++ src/components/ReportCategoryBtn/ReportCategoryBtn.jsx
@@ -1,17 +1,24 @@
 import s from './ReportCategoryBtn.module.css';
 import svg from '../../svgReport/svg-report.svg';
+import iconData from '../../data.json';
 
-export const ReportCategoryBtn = ({ name, sum, icon }) => {
+export const ReportCategoryBtn = ({ data }) => {
+  const icon = iconData.reduce((str, el) => {
+    if (el.name === data[0]) {
+      str = el.icon;
+    }
+    return str;
+  }, '');
   return (
     <li className={s.categryItem}>
-      <p className={s.categorySum}>{sum}</p>
+      <p className={s.categorySum}>{data[1].total}</p>
       <button className={s.btn}>
         <svg className={s.btnIco} width={56} height={56}>
           <use href={`${svg}#${icon}`}></use>
         </svg>
         <div className={s.btnBg}></div>
       </button>
-      <p className={s.categoryName}>{name}</p>
+      <p className={s.categoryName}>{data[0]}</p>
     </li>
   );
 };
