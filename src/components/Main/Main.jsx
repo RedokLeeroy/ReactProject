979d07d8b2c8e9457ff645a86aaa4cef1153b7d4
andrii_h <galgayand@gmail.com>
2022-08-19 18:09:06 +0300

    Update Main.jsx

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,10 @@
 import Button from 'components/common/button/button';
 import { SummaryTable } from 'components/SummaryTable/SummaryTable';
 import { TransactionTable } from 'components/TransactionTable/TransactionTable';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import s from './Main.module.css';
 import { useDispatch, useSelector } from 'react-redux';
-import { expensePost } from 'redux/expense/expense-operations';
+import { expenseGet, expensePost } from 'redux/expense/expense-operations';
 import { NavLink, useLocation } from 'react-router-dom';
 import { incomePost } from 'redux/income/income-operations';
 import { Calendar } from 'components/Calendar/Calendar';
@@ -23,8 +23,15 @@ export const Main = () => {
   const incomesTransactionData = useSelector(({ income }) => income.incomes);
   const expensesSummaryData = useSelector(({ expense }) => expense.monthsStats);
   const incomesSummaryData = useSelector(({ income }) => income.monthsStats);
+  const balance = useSelector(({ balance }) => balance);
   const dispatch = useDispatch();
   const pageLocation = useLocation().pathname;
+
+  console.log(balance);
+
+  useEffect(() => {
+    dispatch(expenseGet());
+  }, [dispatch, balance]);
 
   let products;
   let transactionData;
