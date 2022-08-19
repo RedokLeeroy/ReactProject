b1910c4c12557fea683465be4e345e0066f4fd84
alyapolishchuk <alinochkarul081@gmail.com>
2022-08-19 15:09:30 +0300

    с

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,8 +2,6 @@ import { createPortal } from 'react-dom';
 import css from './Modal.module.css';
 import sprite from '../../images/sprite.svg';
 
-//клікабельність кнопок треба додавати тут??
-
 export default function Modal({ title, onSubmitButton, onRejectButton }) {
 
   return createPortal(
