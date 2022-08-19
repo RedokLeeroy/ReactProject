91b52636bb347a9988d04d992282a732b148360e
alyapolishchuk <alinochkarul081@gmail.com>
2022-08-19 15:08:15 +0300

    c

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,15 +2,12 @@ import { createPortal } from 'react-dom';
 import css from './Modal.module.css';
 import sprite from '../../images/sprite.svg';
 
-export default function Modal({
-  title = 'Do you really want to leave?',
-  onSubmitButton,
-  onRejectButton,
-}) {
+export default function Modal({ title, onSubmitButton, onRejectButton }) {
+
   return createPortal(
     <div className={css.backdrop}>
       <div className={css.modal}>
-        <button className={css.closeButton} type="button">
+        <button className={css.onRejectButton} type="button">
           <svg className={css.svg} width={12} height={12}>
             <use href={`${sprite}#close`}></use>
           </svg>
