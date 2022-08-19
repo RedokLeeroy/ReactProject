22240f03af29c1eecc5356bf909818143d7a26f5
Oleksandr Abramov <aleksander.abramov@gmail.com>
2022-08-19 18:39:47 +0300

    fix date

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -95,6 +95,10 @@ export const Main = () => {
     }
   };
 
+  const setDate = date => {
+    setDatePicker(date.toISOString());
+  };
+
   return (
     <div className={s.container}>
       <nav>
@@ -119,7 +123,7 @@ export const Main = () => {
         <div className={s.formContainer}>
           <div className={s.calendar}>
             <Calendar
-              setDate={setDatePicker}
+              setDate={setDate}
               startDate={startDate}
               setStartDate={setStartDate}
             />
