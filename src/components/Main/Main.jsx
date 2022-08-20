058b31b2a92ebc361b577c353840d6004ec5ca16
andrii_h <galgayand@gmail.com>
2022-08-20 16:46:54 +0300

    table done

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -150,99 +150,112 @@ export const Main = () => {
         </NavLink>
       </nav>
       <div className={s.contentContainer}>
-        <div className={s.formContainer}>
-          <div className={s.calendar}>
-            <Calendar
-              setDate={setDate}
-              startDate={startDate}
-              setStartDate={setStartDate}
-            />
-          </div>
-          <form className={s.form} onSubmit={handleSubmitForm}>
-            <input
-              className={s.inputDescription}
-              placeholder="Product description"
-              autoComplete="off"
-              type="text"
-              name="description"
-              value={description}
-              onChange={handleChangeForm}
-            />
-
-            <div className={s.inputCategoryContainer}>
-              <button
-                className={s.inputCategory}
-                onClick={handleIsListTogle}
-                type="button"
-              >
-                {category ? (
-                  <p style={{ color: '#52555F' }}>{category}</p>
-                ) : (
-                  <p style={{ color: '#c7ccdc' }}>Product category</p>
-                )}
-                <span className={s.arrow}>&#129171;</span>
-              </button>
-              {list && (
-                <>
-                  <div className={s.overlay} onClick={handleCloseByDrope}></div>
-                  <ul className={s.listCategory}>
-                    {products.map((el, ind) => (
-                      <li
-                        value={el}
-                        key={ind}
-                        className={s.itemCategory}
-                        onClick={() => {
-                          setCategory(el);
-                          handleIsListTogle();
-                        }}
-                      >
-                        {el}
-                      </li>
-                    ))}
-                  </ul>
-                </>
-              )}
+        <>
+          <div className={s.formContainer}>
+            <div className={s.calendar}>
+              <Calendar
+                setDate={setDate}
+                startDate={startDate}
+                setStartDate={setStartDate}
+              />
             </div>
-            <div className={s.errContainer}>
-              <p className={s.errDescriptionMsg}>
-                {!description && emptyInput && 'Enter description!'}
-              </p>
-              <p className={s.errCategoryMsg}>
-                {!category && emptyInput && 'Select category!'}
-              </p>
-              <p className={s.errSummMsg}>
-                {!sum && emptyInput && 'Enter sum!'}
-              </p>
-            </div>
-            <div className={s.inputSummContainer}>
+            <form className={s.form} onSubmit={handleSubmitForm}>
               <input
-                className={s.inputSumm}
-                placeholder="0,00"
-                type="number"
-                name="sum"
-                value={sum}
+                className={s.inputDescription}
+                placeholder="Product description"
+                autoComplete="off"
+                type="text"
+                name="description"
+                value={description}
                 onChange={handleChangeForm}
               />
-              <Calculator />
-            </div>
-          </form>
-          <div className={s.buttonContainer}>
-            <Button text={'INPUT'} type={'submit'} onClick={handleSubmitForm} />
-            <Button text={'CLEAR'} type={'button'} onClick={handleResetForm} />
-          </div>
-        </div>
 
-        <div className={s.tableContainer}>
-          <div className={s.prods}>
-            <TransactionTable
-              transactionData={transactionData}
-              tablePage={pageLocation}
-            />
+              <div className={s.inputCategoryContainer}>
+                <button
+                  className={s.inputCategory}
+                  onClick={handleIsListTogle}
+                  type="button"
+                >
+                  {category ? (
+                    <p style={{ color: '#52555F' }}>{category}</p>
+                  ) : (
+                    <p style={{ color: '#c7ccdc' }}>Product category</p>
+                  )}
+                  <span className={s.arrow}>&#129171;</span>
+                </button>
+                {list && (
+                  <>
+                    <div
+                      className={s.overlay}
+                      onClick={handleCloseByDrope}
+                    ></div>
+                    <ul className={s.listCategory}>
+                      {products.map((el, ind) => (
+                        <li
+                          value={el}
+                          key={ind}
+                          className={s.itemCategory}
+                          onClick={() => {
+                            setCategory(el);
+                            handleIsListTogle();
+                          }}
+                        >
+                          {el}
+                        </li>
+                      ))}
+                    </ul>
+                  </>
+                )}
+              </div>
+              <div className={s.errContainer}>
+                <p className={s.errDescriptionMsg}>
+                  {!description && emptyInput && 'Enter description!'}
+                </p>
+                <p className={s.errCategoryMsg}>
+                  {!category && emptyInput && 'Select category!'}
+                </p>
+                <p className={s.errSummMsg}>
+                  {!sum && emptyInput && 'Enter sum!'}
+                </p>
+              </div>
+              <div className={s.inputSummContainer}>
+                <input
+                  className={s.inputSumm}
+                  placeholder="0,00"
+                  type="number"
+                  name="sum"
+                  value={sum}
+                  onChange={handleChangeForm}
+                />
+                <Calculator />
+              </div>
+            </form>
+            <div className={s.buttonContainer}>
+              <Button
+                text={'INPUT'}
+                type={'submit'}
+                onClick={handleSubmitForm}
+              />
+              <Button
+                text={'CLEAR'}
+                type={'button'}
+                onClick={handleResetForm}
+              />
+            </div>
           </div>
-          <div className={s.sumary}>
-            <SummaryTable summaryData={summaryData} />
+
+          <div className={s.tableContainer}>
+            <div className={s.prods}>
+              <TransactionTable
+                transactionData={transactionData}
+                tablePage={pageLocation}
+              />
+            </div>
+            <div className={s.sumary}>
+              <SummaryTable summaryData={summaryData} />
+            </div>
           </div>
-        </div>
+        </>
       </div>
     </div>
   );
