2b8b1fa7cc68855ca01d30b5f1818231f07e4ee5
andrii_h <galgayand@gmail.com>
2022-08-20 16:57:28 +0300

    лінтер матюкається

--- src/components/BalanceContainer/Balance/Balance.jsx
+++ src/components/BalanceContainer/Balance/Balance.jsx
@@ -1,7 +1,7 @@
-import ReportButton from 'components/BalanceContainer/ReportButton/ReportButton';
-import PropTypes from 'prop-types';
-import ButtonBack from '../ButtonBack/ButtonBack';
-import SliderDate from '../SliderDate/SliderDate';
+// import ReportButton from 'components/BalanceContainer/ReportButton/ReportButton';
+// import PropTypes from 'prop-types';
+// import ButtonBack from '../ButtonBack/ButtonBack';
+// import SliderDate from '../SliderDate/SliderDate';
 import s from './Balance.module.css';
 
 const btnBack = false;
@@ -9,24 +9,27 @@ const btnBack = false;
 const Balance = props => {
   return (
     <>
-    <div className={btnBack?s.containerWithBtnBack:s.containerWithOutBtnBack}>
-
+      <div
+        className={btnBack ? s.containerWithBtnBack : s.containerWithOutBtnBack}
+      >
         <p className={s.text}>Balance: </p>
         <div className={s.inpuWrap}>
           <input
-          className={btnBack?s.balanceContainer:s.balanceContainerWithOutBtn}
-          // value={balance}
-          placeholder='0,00 UAH'
-        />
-      <button className={btnBack? s.btnWithBtnBack:s.btn}>Confirm</button>
+            className={
+              btnBack ? s.balanceContainer : s.balanceContainerWithOutBtn
+            }
+            // value={balance}
+            placeholder="0,00 UAH"
+          />
+          <button className={btnBack ? s.btnWithBtnBack : s.btn}>
+            Confirm
+          </button>
         </div>
-
-
-        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-Balance.propTypes = {}
+Balance.propTypes = {};
 
-export default Balance
+export default Balance;
--- src/components/BalanceContainer/BalanceContainer.jsx
+++ src/components/BalanceContainer/BalanceContainer.jsx
@@ -1,28 +1,26 @@
-import PropTypes from 'prop-types'
-import Balance from './Balance/Balance'
-import ButtonBack from './ButtonBack/ButtonBack'
-import ReportButton from './ReportButton/ReportButton'
+// import PropTypes from 'prop-types'
+import Balance from './Balance/Balance';
+import ButtonBack from './ButtonBack/ButtonBack';
+import ReportButton from './ReportButton/ReportButton';
 import SliderDate from './SliderDate/SliderDate';
-import s from './BalanceContainer.module.css'
+import s from './BalanceContainer.module.css';
 
 const slider = true;
 const btnBack = true;
 
 const BalanceContainer = props => {
   return (
-  <>
-    
-    <div className={s.container}>
-      
-{btnBack && <ButtonBack />}
+    <>
+      <div className={s.container}>
+        {btnBack && <ButtonBack />}
         <Balance />
- {slider || <ReportButton />}
-        <SliderDate/>
+        {slider || <ReportButton />}
+        <SliderDate />
       </div>
-      </>
-  )
-}
+    </>
+  );
+};
 
-BalanceContainer.propTypes = {}
+BalanceContainer.propTypes = {};
 
-export default BalanceContainer
+export default BalanceContainer;
