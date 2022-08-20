287b48ab9107c53704647d2f0a16f23218cd1ec4
Tasha1003Natasha <0955712824n@gmail.com>
2022-08-20 21:25:44 +0300

    Лого  logOut

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,18 +1,27 @@
 import styles from './Navigation.module.css';
 import Sprite from '../../images/sprite.svg';
-import { useSelector } from 'react-redux';
 import UserMenu from 'components/UserMenu/UserMenu';
 import { getIsLoggedIn } from 'redux/auth/auth-selector';
 
+import { useDispatch, useSelector } from 'react-redux';
+import { logOut } from 'redux/auth/auth-operations';
+
 const Navigation = () => {
   const isLogin = useSelector(getIsLoggedIn);
+  const dispatch = useDispatch();
 
   return (
     <header className={styles.header}>
       <div className={styles.headerContainer}>
-        <svg className={styles.logoIcon} width={90} height={31}>
-          <use href={`${Sprite}#icon-logo`}></use>
-        </svg>
+        <button
+          className={styles.headerButton}
+          type="button"
+          onClick={() => dispatch(logOut())}
+        >
+          <svg className={styles.logoIcon} width={90} height={31}>
+            <use href={`${Sprite}#icon-logo`}></use>
+          </svg>
+        </button>
         {isLogin && <UserMenu />}
       </div>
     </header>
