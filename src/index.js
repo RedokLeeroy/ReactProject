6ade413ca2f380cef5f465a5f295ff9e216ac2f0
RedokLeeroy <Lys1999@ukr.net>
2022-08-21 21:50:18 +0300

    .

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { BrowserRouter } from 'react-router-dom';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <PersistGate loading={<div>...loading</div>} persistor={persistor}>
-      <BrowserRouter>
+      <BrowserRouter basename="https://jocular-croquembouche-65fdcf.netlify.app/">
         <App />
       </BrowserRouter>
     </PersistGate>
