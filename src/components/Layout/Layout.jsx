b74c8d4c2d1254ef26a323fd2fac7f5abe0ca574
RedokLeeroy <Lys1999@ukr.net>
2022-08-21 23:03:06 +0300

    .

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,13 +1,10 @@
 import Navigation from 'components/Navigation/Navigation';
-import { useSelector } from 'react-redux';
 import { Outlet } from 'react-router';
-import { getIsLoggedIn } from 'redux/auth/auth-selector';
 
 const Layout = () => {
-  const isLogin = useSelector(getIsLoggedIn);
   return (
     <>
-      {isLogin && <Navigation />}
+      <Navigation />
       <Outlet />
     </>
   );
